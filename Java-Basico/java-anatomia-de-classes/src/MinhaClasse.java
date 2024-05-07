67f7feda85c04849c010ba89df67f2d010cc4a14
kauecodify <143859403+kauecodify@users.noreply.github.com>
2024-05-06 23:17:26 -0300

    comitando aulas de sintaxe java

---
+++ Java-Basico/java-anatomia-de-classes/src/MinhaClasse.java
@@ -0,0 +1,62 @@
+public class MinhaClasse {
+    
+    //aula1
+    public static void main (String [] args) {
+        System.out.println("Hello world");
+        
+        //aula2
+
+//variável camelcase: toda classe inicia com sua letra inicial maiúscula. ex> AnoFabricacao
+//variável Uppercase: não pode ter espaços;nomes únicos;@ ou $. ex>kaue_caires
+
+        String BR = "Brazil";
+        FinalString BR = "Brazil";
+        BR = "Brazil";
+
+        int n123_$ = A;
+
+        //aula3
+        //declaração de variáavel > Tipo ; Nome (bem definido) = Atribuição
+        int idade = 23;
+        double altura = 1.62;
+        dog spike;
+        
+        public static void main (String [] args) {
+            String meuNome = "Kaue";
+            int anoFabricacao = 2002;
+
+            boolean verdadeira = false; //boolean atribui verdadeiro ou falso
+        
+            anoFabricacao = 2018;
+
+            //Estrutura - tipo de retorno
+
+            TipoRetorno NomeObjetivoNoInfinito Parametro(s)
+            //exemplo
+            int somar (int numeroUm, int numero2)
+            String formatarCep (long cep)
+
+        }
+
+
+}
+
+}
+//formas de concatenar > 1
+public static main (String [] args) {
+    String primeiroNome = "Kaue";
+    String segundoNome = "Caires";
+    Sprint nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+    
+    System.out.println(nomeCompleto);
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
+    return " Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    //adicionar o parametro e o tipo ou o sinal de mais
+}
+
+        //aula 4 - identação
+        //resumo>termo utilizado para escrever o código hierárquicamente facilitando a visuaalização e o entendimento do programa
+        
