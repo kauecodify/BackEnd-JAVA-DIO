7e98fbba3f9ba4cbb9235e0143b67efbfb9103c8
kauecodify <143859403+kauecodify@users.noreply.github.com>
2024-05-06 23:30:36 -0300

    identação em java

---
+++ Java-Basico/java-anatomia-de-classes/src/BoletimEstudantil.java
@@ -0,0 +1,28 @@
+//S/identação
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 7;
+        if(mediaFinal<6)
+        System.out.println("reprovado");
+        else if(mediaFinal==6)
+        System.out.println("Prova Minerva");
+        else
+        System.out.println("Aprovado");
+        
+    }
+    
+}
+//C/identação
+public class Boletim {
+    public static void main(String[] args) {
+            int mediaFinal = 7;
+                    if(mediaFinal<6)
+        System.out.println("reprovado");
+                        else if(mediaFinal==6)
+            System.out.println("Prova Minerva");
+                            else
+                System.out.println("Aprovado");
+        
+    }
+    
+}
