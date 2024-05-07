a59baff08277fc73b3aa91d6fab4e36dc94e246d
kauecodify <143859403+kauecodify@users.noreply.github.com>
2024-05-06 23:50:33 -0300

    commitAula_1

---
+++ Java-Basico/java-anatomia-de-classes/src/edu/1week/textTabulacao.java
@@ -0,0 +1,8 @@
+//Organização dos arquivos (código fonte) via packages
+//empresas tem segmentos comerciais, organizacionais e opensource
+
+//convenções de aplicabilidade
+//comercial>com
+//organizacional>org
+//opensource>open
+//educação>edu
