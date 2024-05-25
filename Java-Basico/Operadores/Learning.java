7ca20c737e0337d4e1fe3a9cc7cada45670da428
kauecodify <143859403+kauecodify@users.noreply.github.com>
2024-05-25 10:22:50 -0300

    Add files via upload

---
+++ Java-Basico/Operadores/Learning.java
@@ -0,0 +1,38 @@
+public class Learning
+    public static void (String[] args){
+        
+//Operadores Aritméticos
+int a = 10;
+int b = 3;
+int soma = a + b; // soma = 13
+int subtracao = a - b; // subtracao = 7
+int multiplicacao = a * b; // multiplicacao = 30
+int divisao = a / b; // divisao = 3
+int resto = a % b; // resto = 1
+
+//Operadores de Atribuição
+int x = 5;
+x += 3; // x = x + 3 = 8
+x -= 2; // x = x - 2 = 6
+x *= 4; // x = x * 4 = 24
+x /= 3; // x = x / 3 = 8
+x %= 5; // x = x % 5 = 3
+
+//Operadores de Comparação
+int m = 10;
+int n = 5;
+boolean igual = (m == n); // igual = false
+boolean diferente = (m != n); // diferente = true
+boolean maior = (m > n); // maior = true
+boolean menor = (m < n); // menor = false
+boolean maiorOuIgual = (m >= n); // maiorOuIgual = true
+boolean menorOuIgual = (m <= n); // menorOuIgual = false
+
+//Operador Lógico
+boolean p = true;
+boolean q = false;
+boolean resultadoE = (p && q); // resultado(E) = false
+boolean resultadoOu = (p || q); // resultado(Ou) = true
+boolean resultadoNao = !p; // resultado(Nao) = false
+
+}
