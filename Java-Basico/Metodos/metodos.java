940f89d831fa437fcd775e7aa3783b8a20764e2c
kauecodify <143859403+kauecodify@users.noreply.github.com>
2024-05-25 10:38:26 -0300

    Add files via upload

---
+++ Java-Basico/Metodos/metodos.java
@@ -0,0 +1,32 @@
+//
+
+public class Metodos
+public void main (String [] args){
+//metodos classe String
+length(): Retorna o comprimento da string.
+charAt(int index): Retorna o caractere no índice especificado.
+substring(int beginIndex, int endIndex): Retorna uma subcadeia da string.
+indexOf(String str): Retorna o índice da primeira ocorrência da substring especificada.
+toUpperCase(): Retorna uma cópia da string convertida para maiúsculas.
+toLowerCase(): Retorna uma cópia da string convertida para minúsculas.
+
+//metodo classe ArrayList
+add(E element): Adiciona um elemento à lista.
+get(int index): Retorna o elemento no índice especificado.
+size(): Retorna o número de elementos na lista.
+remove(int index): Remove o elemento no índice especificado.
+clear(): Remove todos os elementos da lista.
+
+//Metodo class Math
+abs(double a): Retorna o valor absoluto de um número.
+sqrt(double a): Retorna a raiz quadrada de um número.
+pow(double a, double b): Retorna a base elevada à potência do expoente.
+max(double a, double b): Retorna o maior dos dois números.
+min(double a, double b): Retorna o menor dos dois números.
+
+//metodo class Scanner
+next(): Lê a próxima sequência de caracteres do token do scanner.
+nextInt(): Lê o próximo token de entrada como um inteiro.
+nextLine(): Lê a próxima linha de entrada.
+close(): Fecha o scanner.
+}
